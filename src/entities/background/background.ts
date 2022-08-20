835b03777f91e3d12203364a12a5086ef1db7dab
Morkus Salasevicius <salaseviciusmorkus@gmail.com>
2022-08-20 20:58:18 +0100

    Draw svg

--- src/entities/background/background.ts
+++ src/entities/background/background.ts
@@ -1,24 +1,26 @@
 import { Entity } from "../entities";
 
-import bg from "../../images/bgs/bg 1.svg";
+import Bg from "../../images/bgs/bg 1.svg";
 
 export class Background extends Entity {
+  private svgs: Path2D[] = new Array();
   private images: HTMLImageElement[] = new Array();
 
   constructor() {
     super();
 
-    const svg = new Image(500, 250);
-    svg.src = "../images/bgs/bg 1.svg";
+    const img = new Image();
+    img.src = Bg;
+    console.log(img.width);
 
-    this.images.push(svg);
+    this.images.push(img);
   }
 
   update(): void {}
 
   render(context: CanvasRenderingContext2D): void {
     this.images.forEach((img) => {
-      //   context.drawImage(img, 0, 500);
+      context.drawImage(img, 200, 300, 500, 200);
     });
   }
 }
