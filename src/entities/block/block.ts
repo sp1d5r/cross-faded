4675597f4c64b84fece965bedb90ecdec220349c
Elijah <aa1719@imperial.ac.uk>
2022-08-21 11:07:44 +0100

    Backend: added loop to the blocks.

--- src/entities/block/block.ts
+++ src/entities/block/block.ts
@@ -4,22 +4,35 @@ import { Entity } from "../entities";
 export class Block extends Entity {
     private position: Vector;
     private size: number;
+    private initialPosition: Vector;
 
     constructor(x:number, y:number, size: number) {
         super();
         /* This will initialise variables */
+        this.initialPosition = new Vector(x, y)
         this.position = new Vector(x, y);
         this.size = size;
-        console.log(size)
     }
 
     /* Update and Render */
+
+    getXPosition(): number {
+        return this.position.getX()
+    }
+
+    getYPosition(): number {
+        return this.position.getY()
+    }
 
     updatePostiion(x:number, y:number) {
         this.position.add(new Vector(x, y));
     }
 
     update() {
+        if (this.position.getX() < - this.size){
+            console.log(this.initialPosition)
+            this.position = new Vector(this.initialPosition.getX(), this.initialPosition.getY());
+        }
         /* Update the elements for the player */
         this.updatePostiion(-5, 0);
     }
