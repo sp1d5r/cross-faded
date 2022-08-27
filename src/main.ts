e685a9999b9c40aecd444db16cd8501ca45cc010
Morkus Salasevicius <salaseviciusmorkus@gmail.com>
2022-08-27 15:50:07 +0100

    Register key events

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import "./style.css";
 import { Player } from "./entities/player/player";
 import { Grid } from "./entities/grid/grid";
 import { Background } from "./entities/background/background";
+import { Vector } from "./lib/vector";
 
 // https://medium.com/@KevinBGreene/lets-write-a-physics-based-game-in-typescript-part-1-game-loop-and-simple-physics-4b4cbc0bbdce
 
 const canvas: HTMLCanvasElement = document.createElement("canvas");
 const context: CanvasRenderingContext2D = canvas.getContext("2d")!;
-const root: HTMLElement | null = document.getElementById("root");
+const root: HTMLElement | null = document.body;
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -25,11 +26,33 @@ class Game {
   canvasWidth: number = canvas.width;
   background: Background = new Background(canvas.width, canvas.height);
   grid: Grid = new Grid(this.canvasHeight, this.canvasWidth);
-  tick() {
+
+  constructor() {
+    const player = this.player;
+    const background = this.background;
+    const upVector = new Vector(0, -40);
+    const downVector = new Vector(0, 40);
+
+    // TODO: only allow movement before render() is called()
+    document.onkeydown = function (e) {
+      switch (e.key) {
+        case "ArrowUp":
+          player.move(upVector);
+          background.move(upVector);
+          break;
+        case "ArrowDown":
+          player.move(downVector);
+          background.move(downVector);
+      }
+    };
+  }
+
+  async sleep(m: number) {
+    await new Promise((r) => setTimeout(r, 2000));
+  }
+
+  async tick() {
     this.player.update();
-    if (this.grid.checkColision(this.player)) {
-      return;
-    }
     this.grid.update();
     this.background.update();
 
@@ -38,7 +61,12 @@ class Game {
     this.background.render(context);
     this.grid.render(context);
     this.player.render(context);
-    requestAnimationFrame(()=> this.tick());
+
+    if (this.grid.checkColision(this.player)) {
+      return;
+    }
+
+    requestAnimationFrame(() => this.tick());
   }
 
   clearCanvas(): void {
@@ -48,5 +76,6 @@ class Game {
 
 const game = new Game();
 
-
-requestAnimationFrame(() => {game.tick()})
+requestAnimationFrame(() => {
+  game.tick();
+});
