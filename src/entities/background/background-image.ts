5a35192eac08e6da3a6e461e026933cb1d9bce1b
Morkus Salasevicius <salaseviciusmorkus@gmail.com>
2022-08-27 16:47:18 +0100

    Move background implementations into one file

--- src/entities/background/background-image.ts
+++ src/entities/background/background-image.ts
@@ -1,14 +1,20 @@
 import { Vector } from "../../lib/vector";
 import { Entity } from "../entities";
 
-export abstract class BackgroundImage extends Entity {
+abstract class BackgroundImage extends Entity {
   protected image: HTMLImageElement;
 
   protected position: Vector | null = null;
   protected speed: Vector;
   protected dimensions: Vector;
+  protected initialPosition: Vector;
 
-  constructor(url: string, speed: Vector, dimensions: Vector) {
+  constructor(
+    url: string,
+    speed: Vector,
+    dimensions: Vector,
+    initialPosition: Vector
+  ) {
     super();
     this.image = new Image();
 
@@ -16,6 +22,7 @@ export abstract class BackgroundImage extends Entity {
 
     this.speed = speed;
     this.dimensions = dimensions;
+    this.initialPosition = initialPosition;
   }
 
   async onLoad(): Promise<HTMLImageElement> {
@@ -23,6 +30,10 @@ export abstract class BackgroundImage extends Entity {
       this.image.onload = () => resolve(this.image);
       this.image.onerror = reject;
     });
+  }
+
+  initialisePosition() {
+    this.position = Vector.copy(this.initialPosition);
   }
 
   getDimensions() {
@@ -35,5 +46,69 @@ export abstract class BackgroundImage extends Entity {
 
   getPosition() {
     return this.position;
+  }
+}
+
+export class RepeatingBackgroundImage extends BackgroundImage {
+  protected repeats: number;
+
+  constructor(
+    url: string,
+    speed: Vector,
+    dimensions: Vector,
+    initialPosition: Vector,
+    canvasWidth: number
+  ) {
+    super(url, speed, dimensions, initialPosition);
+    this.repeats = Math.ceil(canvasWidth / dimensions.getX()) + 1;
+  }
+
+  update(): void {
+    if (this.position === null) return;
+    // throw new Error("Update called before image loaded");
+    this.position.add(this.speed);
+
+    if (this.position.getX() <= -this.dimensions.getX()) {
+      const diff = this.position.getX() % this.dimensions.getX();
+      this.setPosition(new Vector(diff, this.position.getY()));
+    }
+  }
+
+  render(context: CanvasRenderingContext2D): void {
+    if (this.position === null) return;
+    // throw new Error("Update called before image loaded");
+    for (let i = 0; i < this.repeats; i++) {
+      context.drawImage(
+        this.image,
+        this.position.getX() + this.dimensions.getX() * i,
+        this.position.getY(),
+        this.dimensions.getX(),
+        this.dimensions.getY()
+      );
+    }
+  }
+}
+
+export class SingleBackgroundImage extends BackgroundImage {
+  update(): void {
+    if (this.position === null) return;
+    // throw new Error("Update called before image loaded");
+    this.position.add(this.speed);
+
+    if (this.position.getX() <= -this.dimensions.getX()) {
+      this.initialisePosition();
+    }
+  }
+
+  render(context: CanvasRenderingContext2D): void {
+    if (this.position === null) return;
+    // throw new Error("Update called before image loaded");
+    context.drawImage(
+      this.image,
+      this.position.getX(),
+      this.position.getY(),
+      this.dimensions.getX(),
+      this.dimensions.getY()
+    );
   }
 }
