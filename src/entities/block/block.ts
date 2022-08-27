4b88ba21434bb30cffc94cb5eac92613ba74005a
Morkus Salasevicius <salaseviciusmorkus@gmail.com>
2022-08-27 15:50:47 +0100

    formatting

--- src/entities/block/block.ts
+++ src/entities/block/block.ts
@@ -2,60 +2,69 @@ import { Vector } from "../../lib/vector";
 import { Entity } from "../entities";
 
 export class Block extends Entity {
-    private position: Vector;
-    private size: number;
-    private initialPosition: Vector;
-    private active: boolean;
-    private probability: number;
+  private position: Vector;
+  private size: number;
+  private initialPosition: Vector;
+  private active: boolean;
+  private probability: number;
 
-    constructor(x:number, y:number, size: number, canvasWidth: number) {
-        super();
-        /* This will initialise variables */
-        this.initialPosition = new Vector(canvasWidth, y)
-        this.position = new Vector(x, y);
-        this.size = size;
-        this.active = Math.floor(Math.random() * 100) < 2;
-        this.probability = 2; /* % a tile becomes active*/
+  constructor(x: number, y: number, size: number, canvasWidth: number) {
+    super();
+    /* This will initialise variables */
+    this.initialPosition = new Vector(canvasWidth, y);
+    this.position = new Vector(x, y);
+    this.size = size;
+    this.active = Math.floor(Math.random() * 100) < 2;
+    this.probability = 2; /* % a tile becomes active*/
+  }
+
+  _initialseAgain(diff: number) {
+    this.position = new Vector(
+      this.initialPosition.getX() + diff,
+      this.initialPosition.getY()
+    );
+    this.probability += 2;
+    this.active = Math.floor(Math.random() * 100) < this.probability;
+  }
+
+  /* Update and Render */
+
+  getXPosition(): number {
+    return this.position.getX();
+  }
+
+  getYPosition(): number {
+    return this.position.getY();
+  }
+
+  updatePosition(x: number, y: number) {
+    this.position.add(new Vector(x, y));
+  }
+
+  isActive() {
+    return this.active;
+  }
+
+  update() {
+    if (this.position.getX() < -this.size) {
+      let diff = this.size + this.position.getX();
+      this._initialseAgain(diff);
+    } else {
+      /* Update the elements for the player */
+      this.updatePosition(-4, 0);
     }
+  }
 
-    _initialseAgain(diff: number) {
-        this.position = new Vector(this.initialPosition.getX() + diff, this.initialPosition.getY());
-        this.probability += 2;
-        this.active = Math.floor(Math.random() * 100) < this.probability;
-    }
-
-
-    /* Update and Render */
-
-    getXPosition(): number {
-        return this.position.getX()
-    }
-
-    getYPosition(): number {
-        return this.position.getY()
-    }
-
-    updatePostiion(x:number, y:number) {
-        this.position.add(new Vector(x, y));
-    }
-
-    isActive() {
-        return this.active;
-    }
-
-    update() {
-        if (this.position.getX() < - this.size){
-            let diff = this.size + this.position.getX();
-            this._initialseAgain(diff);
-        } else {
-            /* Update the elements for the player */
-            this.updatePostiion(-4, 0);
-        }
-    }
-
-    render(context: CanvasRenderingContext2D) {
-        /* Render the changes made */
-        context.fillStyle= this.active ? "rgba(255,0,0,0.05)": "rgba(255,255,255, 0.05)";
-        context.fillRect(this.position.getX(), this.position.getY(), this.size, this.size);
-    }
+  render(context: CanvasRenderingContext2D) {
+    /* Render the changes made */
+    context.fillStyle = this.active
+      ? "rgba(255,0,0,0.05)"
+      : "rgba(255,255,255, 0.05)";
+    context.fillRect(
+      this.position.getX(),
+      this.position.getY(),
+      this.size,
+      this.size
+    );
+  }
 }
